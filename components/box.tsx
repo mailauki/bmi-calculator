8a45dd1d3285ce7af7379ba817024442c69d66ad
Julie Evans <juliemevans17@gmail.com>
2025-01-01 16:47:17 -0600

    Add counter to buttons

--- components/box.tsx
+++ components/box.tsx
@@ -1,17 +1,26 @@
+"use client"
 import { MinusIcon, PlusIcon } from "@heroicons/react/24/outline";
 import RoundButton from "./round-button";
-
+import { useState } from "react";
 
 export default function Box() {
+	const [count, setCount] = useState(0)
+	function addCount() {
+		if (count >= 0) setCount(count+1)
+	}
+	function minusCount() {
+		if (count > 0) setCount(count-1)
+	}
+
 	return (
 		<div className="m-6 p-6 flex flex-col justify-between items-center gap-6 rounded-2xl shadow-[20px_20px_60px_#c4c3c2,-20px_-20px_60px_#ffffff]">
 			<p className="text-2xl">Box</p>
-			<p className="text-7xl font-extralight">42</p>
+			<p className="text-7xl font-extralight">{count}</p>
 			<div className="mb-3 flex justify-center gap-12 w-full">
-				<RoundButton>
+				<RoundButton onClick={addCount}>
 					<PlusIcon className="size-6 text-current" />
 				</RoundButton>
-				<RoundButton>
+				<RoundButton onClick={minusCount}>
 					<MinusIcon className="size-6 text-current" />
 				</RoundButton>
 			</div>
--- components/round-button.tsx
+++ components/round-button.tsx
@@ -1,12 +1,15 @@
+import { MouseEventHandler } from "react";
+
 export default function RoundButton({
   children,
+	onClick,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}> & { onClick: MouseEventHandler<HTMLButtonElement> }) {
 	return (
 		<button
+			onClick={onClick}
 			className="p-6 rounded-full hover:bg-stone-300/50 shadow-[20px_20px_60px_#c4c3c2,-20px_-20px_60px_#ffffff]"
-			// className="p-6 rounded-full bg-gradient-[145deg] from-[#f7f5f4] to-[#d0cecd] shadow-[20px_20px_60px_#c4c3c2,-20px_-20px_60px_#ffffff]"
 		>
 			{children}
 		</button>
