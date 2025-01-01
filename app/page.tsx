277f9f867e0998fb7bd3b4149995963ca18317f8
Julie Evans <juliemevans17@gmail.com>
2025-01-01 15:37:54 -0600

    Add Neumorphism box

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
+import Box from "@/components/box";
+
 export default function Home() {
 	return (
-		<div
-			className="grid sm:grid-rows-[10%_auto_10%] grid-rows-[10%_10%_auto_10%_10%] min-h-screen text-center font-[family-name:var(--font-geist-sans)]"
-			// className="grid sm:grid-rows-[80px_auto_80px] sm:grid-cols-[20%_auto_20%] grid-rows-[80px_80px_auto_80px_80px] grid-cols-1 h-screen"
-		>
-			<header className="bg-indigo-200 sm:col-start-1 sm:col-end-5 col-start-auto col-end-auto">Header</header>
-			<aside className="bg-amber-200">Left</aside>
-			<main className="bg-green-200 sm:col-span-2">Main</main>
-			<aside className="bg-amber-200">Right</aside>
-			<footer className="bg-indigo-200 sm:col-start-1 sm:col-end-5 col-start-auto col-end-auto">Footer</footer>
+		<div className="grid sm:grid-rows-[10%_auto_10%] grid-rows-[10%_10%_auto_10%_10%] min-h-screen bg-stone-200 text-center font-[family-name:var(--font-geist-sans)]">
+			<header className="sm:col-start-1 sm:col-end-5 col-start-auto col-end-auto">Header</header>
+			<aside>Left</aside>
+			<main className="sm:col-span-2">
+				Main
+				<Box/>
+			</main>
+			<aside>Right</aside>
+			<footer className="sm:col-start-1 sm:col-end-5 col-start-auto col-end-auto">Footer</footer>
 		</div>
 	)
 }
---
+++ components/box.tsx
@@ -0,0 +1,5 @@
+export default function Box() {
+	return (
+		<div className="m-6 p-6 rounded-2xl shadow-[20px_20px_60px_#c4c3c2,-20px_-20px_60px_#ffffff]">Box</div>
+	)
+}
