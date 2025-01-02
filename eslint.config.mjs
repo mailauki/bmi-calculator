7195eaab9d8e9c17b505c796e4d3a5e5abdba582
Julie Evans <juliemevans17@gmail.com>
2025-01-01 19:56:00 -0600

    Update lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import js from '@eslint/js'
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -7,10 +8,17 @@ const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ['eslint:recommended', 'next/core-web-vitals', 'next/typescript', 'prettier', 'plugin:@next/next/recommended'],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+    },
+  }),
 ];
 
 export default eslintConfig;
