1498d062ff8ceb711ed07c1757a361ae12dced9e
Julie Evans <juliemevans17@gmail.com>
2025-01-02 21:40:29 -0600

    Add bmi percentage

--- app/results/bmi.tsx
+++ app/results/bmi.tsx
@@ -1,4 +1,9 @@
 export default function BMI({ number }: { number: number }) {
+  const percent = (number*100)/40
+  const offset = `${ percent >= 100 ? 0 : number <= 18.5 ? 100 : 100-percent+46.25 }`
+  // offset accounts for the maximum percentage or bmi over 40; 
+  // and the minimum percentage (46.25%) or bmi under 18.5
+	
   return (
     <div className="p-4 w-fit h-fit rounded-full shadow-[20px_20px_60px_#c4c3c2,-20px_-20px_60px_#ffffff] dark:shadow-[20px_20px_60px_#231f1f,-20px_-20px_60px_#2f2b29]">
       <div className="relative size-52">
@@ -6,7 +11,7 @@ export default function BMI({ number }: { number: number }) {
           {/* Background Circle */}
           <circle className="stroke-current text-stone-300 dark:text-neutral-700" cx="18" cy="18" fill="none" r="16" strokeWidth="2" />
           {/* Progress Circle */}
-          <circle className="stroke-current text-blue-500 dark:text-blue-400" cx="18" cy="18" fill="none" r="16" strokeDasharray="100" strokeDashoffset="65" strokeLinecap="round" strokeWidth="2" />
+          <circle className="stroke-current text-blue-500 dark:text-blue-400" cx="18" cy="18" fill="none" r="16" strokeDasharray="100" strokeDashoffset={offset} strokeLinecap="round" strokeWidth="2" />
         </svg>
 
         {/* Percentage Text */}
